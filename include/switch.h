1a6a6792bdf539b5b2ef447bd8b0c881b3ee97eb
Serguei Okladnikov <oklaspec@gmail.com>
2009-05-21 11:42:38 +0300

    <empty message>

--- include/switch.h
+++ include/switch.h
@@ -1,9 +1,6 @@
 #include <string.h>
 
-#ifdef _______cplusplus
-
-
-#else // __cplusplus
+#ifdef __cplusplus
 
 struct SWITCH__D_A_T_A
   {
@@ -40,7 +37,6 @@ struct SWITCH__D_A_T_A
 	}
   };
 
-
 #define SWITCH(arg) if(1){SWITCH__D_A_T_A switch__d_a_t_a; \
  switch__d_a_t_a.strPtrThrSw=arg;switch__d_a_t_a.bEnterDefault=true;if(switch__d_a_t_a.cmp(
 
@@ -54,5 +50,32 @@ struct SWITCH__D_A_T_A
 
 #define END         }};
 
+#else // not defined __cplusplus
 
-#endif // __cplusplus
+typedef struct tagSWITCH__D_A_T_A
+  {
+  int bEnterFall;
+  int bEnterDefault;
+  const char* strPtrThrSw;
+  } SWITCH__D_A_T_A;
+
+  int SWITCH__D_A_T_A_cmp(SWITCH__D_A_T_A* data, const char*cnst, int ndeflt);
+  int SWITCH__D_A_T_A_fall(SWITCH__D_A_T_A* data, const char* cnst, int ndeflt);
+
+#define SWITCH(arg) if(1){SWITCH__D_A_T_A switch__d_a_t_a; \
+ switch__d_a_t_a.strPtrThrSw=arg; \
+ switch__d_a_t_a.bEnterDefault=1; \
+ if(SWITCH__D_A_T_A_cmp(&switch__d_a_t_a,
+
+#define CASE(cnst)  cnst,1)){
+
+#define BREAK       switch__d_a_t_a.bEnterFall=0;}else if(SWITCH__D_A_T_A_cmp(&switch__d_a_t_a,
+
+#define FALL        }if(SWITCH__D_A_T_A_fall(&switch__d_a_t_a,
+
+#define DEFAULT     NULL,0)){
+
+#define END         }};
+
+
+#endif // defined __cplusplus / not defined __cplusplus
